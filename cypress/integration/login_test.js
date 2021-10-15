6da848c4fc51c74a3225a3c4d3059a24a99c525c
TaChyla <tachylad.murray@gmail.com>
2021-10-14 23:54:50 -0700

    expect string can be better

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -33,9 +33,8 @@ describe("SignUp Flow", function() {
     cy.get(".signupInputField")
 
 //    click name field & type
-    .type("fake14email@noemail.com")
-//    iterate #
-    .should("have.value", "fake14email@noemail.com")
+    .type("fake45email@noemail.com")
+    .should("have.value", "fake45email@noemail.com")
 //    click continue
     cy.contains("Continue").click()
 
@@ -45,7 +44,7 @@ describe("SignUp Flow", function() {
     .type("Fake Name")
 //    add a wait time
     cy.wait(100)
-    cy.get('.block > .signupInputField')
+    cy.get(".block > .signupInputField")
         expect("Full Name").to.exist
 
 //    other POTENTIAL tests
@@ -60,39 +59,60 @@ describe("SignUp Flow", function() {
     cy.get(".relative > input")
         assert.isNotNull("input > value")
 
+//    other POTENTIAL tests
 //should contain X characters
 //not be visible for security reasons
 
     cy.get('.flex > .rounded').click()
-    cy.url()
-        .should("include", "https://www.airtable.com")
+    cy.url().should("include", "https://www.airtable.com")
 
-        //        Next Steps
-//        Get (Sign In identifier), Click (Sign In button)
+
+//        Share base with collaborator by using “Invite by email” flow
+//        Get (Sign In identifier)
+//        Click (Sign In button)
           cy.get('.css-10n548d').click()
-          cy.url()
-            .should("include", "/login")
 
 //        Interact with form to input email
 //        Interact with form to input password
-
-          cy.get(":nth-child(1) > .signupInputField").type("fake14email@noemail.com")
+          cy.get(":nth-child(1) > .signupInputField").type("fake45email@noemail.com")
           cy.get(":nth-child(2) > .signupInputField").type("Z987654321#")
 
 //        Click Login button
           cy.get(".py1-and-half").click()
+
 //      Validate user is logged in
-//        adjust settings
-//        skip
-//        skip
-//        Share base with collaborator by using “Invite by email” flow
-//        Set the permission level as “Editor”
-//        Verify that the newly collaborated user email is displayed under “Base Collaborators”
-//        Verify that the collaborator has “Editor” role displayed under “Base Collaborators”
 
+          //Click share button
+          cy.get(".justify-end > :nth-child(1) > .pointer").click()
+
+          //Share by email is selected by default
+          //Interact with Input text field Should Exist
+          cy.get("span.flex > .white > .flex").type("EmailInvitedUser@noemail.com")
+
+          //Click Creator dropdown
+          cy.get(".flex.flex-none > .selectMenu > .focus-container > .items-center > .flex-auto").click()
+
+          //Select Editor permission
+          cy.get(".hdropdown > :nth-child(3) > :nth-child(1)").click()
+
+          //Click send invite button
+          cy.get('[style="width: 112px;"] > .blue').click()
+          cy.wait(500)
+
+          //Validate new user displays
+          //.contains("This workspace has 2 collaborators.")
+          cy.get(".overflow-auto")
+            expect(".overflow-auto > :nth-child(2) > :nth-child(5)").to.exist
+            expect(".overflow-auto > :nth-child(2) > :nth-child(4)").to.exist
+
+          cy.get(".overflow-auto > :nth-child(2) > :nth-child(4) > :nth-child(3) > :nth-child(1) > .selectMenu > .focus-container > .items-center")
+            expect("Editor")
+
+//        Create base
+//        skip
+//        skip
+//        Validate Base
 //        add to README setup instructions
-
-//if role = "button" with text "Start a Base" exists click()
     })
 })
 
