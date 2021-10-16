be4acc046143c89e8bddb48efc889eaa8bc303b2
TaChyla <tachylad.murray@gmail.com>
2021-10-15 17:15:30 -0700

    Signup FLow Complete

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-Thank you Airtable team for allowing me the opportunity to interview for this role. My name is Tachyla Murray
+Thank you, Airtable team for allowing me the opportunity to interview for this role. 
+My name is TaChyla Murray and I thrive in roles where I'm able to combine my
+7 years experience manual testing with my ability to write JavaScript code. I found
+this take home assessment as an exciting opportunity to push the envelope and learn innovative testing
+methodology. In my Lyft role, I frequently used Espresso testing framework and Java to contribute Android automated tests. 
+This was my first time implementing Cypress, and it really challenged me but relying 
+on my Javascript experience helped me to successfully write one automation test mocking the Sign Up Flow.
 
+//        add to README setup instructions
+Install npm
+npm install Cypress version
+
+From the airtable-cypress-take-home-assignment folder navigate to the login_test.jsfile
+
+Open the "cypress" folder
+Open the "integration" folder
+Invoke the excutable to startup the cypress UI. Use Terminal Command: node_modules/.bin/cypress open
+Tests that exist in the Integration Tests Folder will display
+Double click login_test.js to begin the SignUp Flow test. 
+UI interface will open & run login_test.js executing all the tests it includes unless instructed otherwise.  
--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -1,30 +1,8 @@
 import "@testing-library/cypress/add-commands.js";
 import "@testing-library/dom/dist/queries/label-text.js";
 
-//REMEMBER TO
-// Consider assesibility
-//add error cases to these methods
-//Refactor to not repeat
-
-describe("LoginPage", function(){
-    it("Visits login page", function(){
-        cy.visit("https://www.airtable.com/login")
-        cy.contains("create")
-    })
-})
-
-describe("SignUp Page", function() {
-    it("Click signup button", function() {
-        cy.visit("https://www.airtable.com/login")
-        cy.contains("create an account").click()
-                cy.url()
-               .should("include", "/signup")
-
-    })
-})
-
 describe("SignUp Flow", function() {
-    it.only("Interact with Form", function() {
+    it("Interact with Form", function() {
     cy.visit("https://www.airtable.com/login")
     cy.contains("create an account").click()
 
@@ -32,60 +10,55 @@ describe("SignUp Flow", function() {
         .should("include", "/signup")
     cy.get(".signupInputField")
 
-//    click name field & type
-    .type("fake45email@noemail.com")
-    .should("have.value", "fake45email@noemail.com")
-//    click continue
+//  click name field & type then click continue
+    .type("fakeinformation@noemail.com")
+    .should("have.value", "fakeinformation@noemail.com")
     cy.contains("Continue").click()
 
-//assert full name
+//  assert full name
     cy.get("#signUpForm")
     .contains("Full Name")
     .type("Fake Name")
-//    add a wait time
+//  wait for input to populate
     cy.wait(100)
     cy.get(".block > .signupInputField")
         expect("Full Name").to.exist
 
-//    other POTENTIAL tests
-//    field should not allow numbers
-// should contain at least X characters
-
-//assert Password
+//  assert Password
     cy.get("#signUpForm")
     .contains("Password").type("Z987654321#")
-//    add a wait time
+//   wait for input to populate
     cy.wait(100)
     cy.get(".relative > input")
         assert.isNotNull("input > value")
 
-//    other POTENTIAL tests
-//should contain X characters
-//not be visible for security reasons
-
-    cy.get('.flex > .rounded').click()
+    cy.get(".flex > .rounded").click()
     cy.url().should("include", "https://www.airtable.com")
 
-
 //        Share base with collaborator by using “Invite by email” flow
-//        Get (Sign In identifier)
-//        Click (Sign In button)
-          cy.get('.css-10n548d').click()
+          cy.get(".css-10n548d").click()
 
 //        Interact with form to input email
 //        Interact with form to input password
-          cy.get(":nth-child(1) > .signupInputField").type("fake45email@noemail.com")
+          cy.get(":nth-child(1) > .signupInputField").type("fakeinformation@noemail.com")
           cy.get(":nth-child(2) > .signupInputField").type("Z987654321#")
 
 //        Click Login button
           cy.get(".py1-and-half").click()
 
-//      Validate user is logged in
+//        Select "Create base" & click base element btn
+          cy.get(":nth-child(1) > .flex-auto > .mb3 > .height-full > .mb1").click()
+          cy.wait(100)
 
-          //Click share button
+//        select BaseNameInputField & type name
+          cy.get(".p2 > .width-full > .flex-auto").type("SampleBaseName")
+//        Assert base, base name displays
+          cy.get("#appTopBarHomeButton").click()
+          cy.get(".flex-wrap > :nth-child(1) > :nth-child(1) > .hover-container").contains("SampleBaseName")
+
+//        Share with collaborator button
           cy.get(".justify-end > :nth-child(1) > .pointer").click()
 
-          //Share by email is selected by default
           //Interact with Input text field Should Exist
           cy.get("span.flex > .white > .flex").type("EmailInvitedUser@noemail.com")
 
@@ -96,29 +69,17 @@ describe("SignUp Flow", function() {
           cy.get(".hdropdown > :nth-child(3) > :nth-child(1)").click()
 
           //Click send invite button
-          cy.get('[style="width: 112px;"] > .blue').click()
+          cy.get("[style='width: 112px;'] > .blue").click()
           cy.wait(500)
 
-          //Validate new user displays
-          //.contains("This workspace has 2 collaborators.")
+//        Validate new user displays
           cy.get(".overflow-auto")
             expect(".overflow-auto > :nth-child(2) > :nth-child(5)").to.exist
             expect(".overflow-auto > :nth-child(2) > :nth-child(4)").to.exist
-
           cy.get(".overflow-auto > :nth-child(2) > :nth-child(4) > :nth-child(3) > :nth-child(1) > .selectMenu > .focus-container > .items-center")
             expect("Editor")
 
-//        Create base
-//        skip
-//        skip
-//        Validate Base
-//        add to README setup instructions
+//            close window ending test
+          cy.get(".rounded-big > .top-0").click()
     })
 })
-
-
-
-//REMEMBER TO
-// Consider assesibility
-//add error cases to these methods
-//Refactor to not repeat
